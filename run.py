d6ec83b0189240bb8a2675e3f3467232e38f51d3
Mathew <gdiskfordocuments@gmail.com>
2020-05-17 21:26:46 +0300

    fff

--- run.py
+++ run.py
@@ -6,13 +6,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
 
+# @app.route('/', methods=['GET', 'POST'])
+# def start():
+#     form = UploadFileModel()
+#     if form.validate_on_submit():
+#         message, flag = make_gender_sentence(form.picture.data, form.picture.data.filename)
+#         flash(message, flag)
+#         return redirect(url_for('start'))
+#     return render_template(
+#         'account.html',
+#         title='Распознать гендерное существо',
+#         form=form
+#     )
+
 @app.route('/', methods=['GET', 'POST'])
 def start():
     form = UploadFileModel()
-    if form.validate_on_submit():
-        message, flag = make_gender_sentence(form.picture.data, form.picture.data.filename)
-        flash(message, flag)
-        return redirect(url_for('start'))
     return render_template(
         'account.html',
         title='Распознать гендерное существо',
